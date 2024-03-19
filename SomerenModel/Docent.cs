e85cba9675bfd5567d0529e5db2f438d41e1e6dd
JasonHHW <jason_wolven@outlook.com>
2024-03-19 11:05:07 +0100

    Ff pushe

---
+++ SomerenModel/Docent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SomerenModel
+{
+    public class Docent
+    {
+        public int DocentId { get; set; }     // database id
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public string Telefoonnummer { get; set; } // LecturerNumber, e.g. 47198
+        public DateTime Geboortedatum { get; set; }
+        public string Kamer {  get; set; }
+
+        public Docent(int docentid, string voornaam, string achternaam, string telefoonnummer, DateTime geboortedatum, string kamer)
+        {
+            DocentId = docentid;
+            Voornaam = voornaam;
+            Achternaam = achternaam;
+            Telefoonnummer = telefoonnummer;
+            Geboortedatum = geboortedatum;
+            Kamer = kamer;
+        }
+     }
+}
--- SomerenModel/Student.cs
+++ SomerenModel/Student.cs
@@ -4,9 +4,21 @@ namespace SomerenModel
 {
     public class Student
     {
-        public int Id { get; set; }     // database id
-        public string Name { get; set; }
-        public int Number { get; set; } // StudentNumber, e.g. 474791
-        public DateTime BirthDate { get; set; }
+        public int StudentId { get; set; }     // database id
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public string Telefoonnummer { get; set; } // StudentNumber, e.g. 474791
+        public string Klas {  get; set; }
+        public string Kamer { get; set; }
+
+        public Student(int studentId, string voornaam, string achternaam, string telefoonnummer, string klas, string kamer)
+        {
+            StudentId = studentId;
+            Voornaam = voornaam;
+            Achternaam = achternaam;
+            Telefoonnummer = telefoonnummer;
+            Klas = klas;
+            Kamer = kamer;
+        }
     }
 }
