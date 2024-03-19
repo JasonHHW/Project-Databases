fc67db0cddf573c4dc9df0efac6183a0eaf50f3d
JasonHHW <jason_wolven@outlook.com>
2024-03-19 11:09:17 +0100

    nog een

--- SomerenModel/Room.cs
+++ SomerenModel/Room.cs
@@ -1,10 +1,18 @@
 ﻿namespace SomerenModel
 {
-    public class Room
+    public class Kamer
     {
-        public int Id { get; set; }         // database id
-        public int Number { get; set; }     // RoomNumber, e.g. 206
-        public int Capacity { get; set; }   // number of beds, either 4, 6, 8, 12 or 16
-        public bool Type { get; set; }      // student = false, teacher = true
+        public string KamerCode { get; set; }         // database id
+        public string Gebouw { get; set; }     // RoomNumber, e.g. 206
+        public int Verdieping { get; set; }   // number of beds, either 4, 6, 8, 12 or 16
+        public bool IsEenPersoons { get; set; }      // student = false, teacher = true
+
+        public Kamer(string kamerCode, string gebouw, int verdieping, bool isEenPersoons)
+        {
+            KamerCode = kamerCode;
+            Gebouw = gebouw;
+            Verdieping = verdieping;
+            IsEenPersoons = isEenPersoons;
+        }
     }
 }
